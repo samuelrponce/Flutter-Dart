9bc0cd2f9732c571d20ec35deb55f3a5edee0d99
Roger Samuel Ponce Rodriguez <ro.rosamuel@hotmail.com>
2023-05-25 13:44:00 -0600

    add a custom button
    add a custom button in the buttons screen

--- widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
+++ widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
@@ -37,34 +37,33 @@ class _ButtonsView extends StatelessWidget {
           alignment: WrapAlignment.center,
           children: [
             ElevatedButton(onPressed: () {}, child: const Text('Elevated')),
-            const ElevatedButton(onPressed: null, child: Text('Elevated Disabled')),
-      
+            const ElevatedButton(
+                onPressed: null, child: Text('Elevated Disabled')),
             ElevatedButton.icon(
-              onPressed: () {},
-              icon: const Icon(Icons.access_alarm_rounded), 
-              label: const Text('Elevated Icon')),
-
-            FilledButton(onPressed: (){}, child: const Text('Filled')),
+                onPressed: () {},
+                icon: const Icon(Icons.access_alarm_rounded),
+                label: const Text('Elevated Icon')),
+            FilledButton(onPressed: () {}, child: const Text('Filled')),
             FilledButton.icon(
-              onPressed: (){}, 
-              icon: const Icon(Icons.accessibility_new),
-              label: const Text('Filled Icon')),
-              
-            OutlinedButton(onPressed: (){}, child: const Text('Outline')),
+                onPressed: () {},
+                icon: const Icon(Icons.accessibility_new),
+                label: const Text('Filled Icon')),
+            OutlinedButton(onPressed: () {}, child: const Text('Outline')),
             OutlinedButton.icon(
-              onPressed: (){}, 
-              icon: const Icon(Icons.terminal),
-              label: const Text('Outline Icon')),
-            
-            TextButton(onPressed: (){}, child: const Text('Text')),
+                onPressed: () {},
+                icon: const Icon(Icons.terminal),
+                label: const Text('Outline Icon')),
+            TextButton(onPressed: () {}, child: const Text('Text')),
+            const CustomButton(),
             TextButton.icon(
-              onPressed: (){}, 
-              icon: const Icon(Icons.account_box_outlined),
-              label: const Text('Text Icon')),
-
-            IconButton(onPressed:(){ }, icon: const Icon(Icons.app_registration_rounded)),
+                onPressed: () {},
+                icon: const Icon(Icons.account_box_outlined),
+                label: const Text('Text Icon')),
             IconButton(
-              onPressed: (){}, 
+                onPressed: () {},
+                icon: const Icon(Icons.app_registration_rounded)),
+            IconButton(
+              onPressed: () {},
               icon: const Icon(Icons.app_registration_rounded),
               style: ButtonStyle(
                 backgroundColor: MaterialStatePropertyAll(colors.primary),
@@ -73,6 +72,27 @@ class _ButtonsView extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Material(
+        color: colors.primary,
+        child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Hola mundo',
+              style: TextStyle(color: Colors.white),
+            )),
       ),
     );
   }
